a2203f9b3991b72b56eda4476dd9d5889df88ac5
Nikita <nikitka_starikov_99@mail.ru>
2020-09-12 14:10:04 +0700

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,18 @@ int main(int argn, char **argv)
 	double a = 0.0;
 	double b = 0.0;
 	double c = 0.0;
-
-	if (argn == 1)
-	{
-		cout << "the number of arguments must be 4" << endl;
-		return -1;
-	}
-	else if (argn == 4)
+	
+	if (argn == 4)
 	{
 		a = atof(argv[1]);
 		b = atof(argv[2]);
 		c = atof(argv[3]);
 
+	}
+	else 
+	{
+		cout << "the number of arguments must be 4" << endl;
+		return -1;
 	}
 
 	auto Pair = make_pair<double, double>(0.0, 0.0);
