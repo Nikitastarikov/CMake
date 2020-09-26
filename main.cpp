a0e309d8c8f819fc39a333f216979a5e104e3d63
nikita <nikitka_starikov_99@mail.ru>
2020-09-26 14:02:38 +0700

    add test

--- main.cpp
+++ main.cpp
@@ -1,46 +1,55 @@
 #include "SimpleConfig.h"
-#ifdef USE_MYLIB
 #include "lib.hpp"
-#endif
 #include <iostream>
 #include <utility>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
+
+void myeducation(double a, double b, double c)
+{
+	auto Pair = make_pair<double, double>(0.0, 0.0);	
+
+	if (!quadratic_equation(a, b, c, Pair))
+	{
+		cout << "x1 is " << Pair.first << endl;
+		cout << "x2 is " << Pair.second << endl;
+	}
+	else 
+	{
+		cout << "iscriminant less than 0, roots are insubstantial" << endl;
+	}
+}
+
 
 int main(int argn, char **argv)
 {
 	double a = 0.0;
 	double b = 0.0;
 	double c = 0.0;
-	
+
+
 	if (argn == 4)
 	{
 		a = atof(argv[1]);
 		b = atof(argv[2]);
 		c = atof(argv[3]);
 
+		myeducation(a, b, c);
+	}
+	else if (argn == 3)
+	{
+		a = atof(argv[1]);
+		b = atof(argv[2]);
+
+		cout << a << " V " << b << " is " << sum(a, b) << endl;
 	}
 	else 
 	{
-		cout << "the number of arguments must be 4" << endl;
+		cout << "./Simple arg1 arg2- arg1 V arg2 is res" << endl;
+		cout << "./Simple arg1 arg2 arg3- quadratic equation" << endl;
 		return -1;
-	}
-
-	auto Pair = make_pair<double, double>(0.0, 0.0);
-
-	cout << endl;
-	cout << a << " + " << b << " = " << sum(a, b) << endl;
-	cout << endl;
-
-	if (!quadratic_equation(a, b, c, Pair))
-	{
-		cout << "x1 = " << Pair.first << endl;
-		cout << "x2 = " << Pair.second << endl;
-	}
-	else 
-	{
-		cout << "iscriminant less than 0, roots are insubstantial" << endl;
 	}
 
 	return 0;
